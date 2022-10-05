93613d03d253d919cd9da78f494ecd2cf27a2b92
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2022-10-05 17:09:23 +0700

    equipped tools swapping added

--- Assets/Script/Inventory/ToolDisplay.cs
+++ Assets/Script/Inventory/ToolDisplay.cs
@@ -2,26 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
-public class InventoryDisplay : MonoBehaviour
+public class ToolDisplay : MonoBehaviour
 {
+    [Header ("Inventory Object")]
     public InventoryObject inventory;
 
+    [Header("Position")]
     public int X_START;//Posisi X awal di panel
     public int Y_START;//Posisi Y awal di panel
-    public int Y_SPACE_BETWEEN_ITEMS; //Jarak Y tiap baris
-    public int NUMBER_OF_ROWS;//Jumlah baris
+
+    private int totalTools;
+    public int currentToolIndex;
+    public GameObject[] tools;
+    public GameObject toolHolder;
+    public GameObject currentTool;
+
+
     Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
         CreateDisplay();
+        totalTools = inventory.Container.Count;
+        tools = new GameObject[totalTools];
+
+        for (int i=0;i < totalTools; i++)
+        {
+            tools[i] = toolHolder.transform.GetChild(i).gameObject;
+            tools[i].SetActive(false);
+        }
+        tools[0].SetActive(true);
+        currentTool = tools[0];
+        currentToolIndex = 0;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         UpdateDisplay();
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            //ke tool selanjutnya
+            if(currentToolIndex < totalTools-1)
+            {
+                tools[currentToolIndex].SetActive(false);
+                currentToolIndex++;
+                tools[currentToolIndex].SetActive(true);
+            }
+            else if(currentToolIndex == totalTools - 1)
+            {
+                tools[currentToolIndex].SetActive(false);
+                currentToolIndex = 0;
+                tools[currentToolIndex].SetActive(true);
+            }
+        }
     }
 
     public void UpdateDisplay()
@@ -33,36 +70,32 @@ public class InventoryDisplay : MonoBehaviour
             if (itemsDisplayed.ContainsKey(inventory.Container[i]))
             {
                 var obj2 = itemsDisplayed[inventory.Container[i]].transform.GetChild(0);
-                obj2.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+                //GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
             }
             //Kalau di container belum ada objectnya maka akan ditambahkan nama object yang diambil beserta amountnya
             else
             {
                 var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
-                var obj2 = obj.transform.GetChild(0);
                 obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
-                obj2.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
                 itemsDisplayed.Add(inventory.Container[i], obj);
             }
         }
     }
 
+
     public void CreateDisplay()
     {
         //Ketika awal mulai scene akan dibuat display dihitung dari Jumlah index container, awal seharusnya kosong karena belum bisa save
-        for(int i  = 0; i < inventory.Container.Count; i++)
+        for (int i = 0; i < inventory.Container.Count; i++)
         {
             var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
-            var obj2 = obj.transform.GetChild(0);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
-            obj2.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
             itemsDisplayed.Add(inventory.Container[i], obj);
         }
     }
 
     public Vector3 GetPosition(int i)
     {
-        return new Vector3(X_START, Y_START + (-Y_SPACE_BETWEEN_ITEMS * (i % NUMBER_OF_ROWS)), 0f);
+        return new Vector3(X_START, Y_START, 0f);
     }
-
 }
