7d253d703b92784c568ee2f124f421130cd41252
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2022-10-08 13:54:58 +0700

    take item fixed

--- Assets/Script/Character/CharacterInventory.cs
+++ Assets/Script/Character/CharacterInventory.cs
@@ -7,17 +7,6 @@ public class CharacterInventory : MonoBehaviour
     public InventoryObject junkInventory;
     public InventoryObject toolInventory;
 
-    public void OnTriggerEnter(Collider other)
-    {
-        var item = other.GetComponent<Item>();
-        if (item)
-        {
-            junkInventory.AddItem(item.item, 1);
-            Destroy(other.gameObject);
-            Debug.Log("E kepencet");
-
-        }
-    }
     private void OnApplicationQuit()
     {
         junkInventory.Container.Clear();
--- Assets/Script/Crafting/Item.cs
+++ Assets/Script/Crafting/Item.cs
@@ -1,8 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Item : MonoBehaviour
 {
     public ItemObject item;
+    public InventoryObject junkInventory;
+
+    public TextMeshProUGUI pickupText;
+
+    private bool pickupAllowed;
+
+    private void Start()
+    {
+        pickupText.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(pickupAllowed && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUp();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player masuk");
+            pickupText.gameObject.SetActive(true);
+            pickupAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player keluar");
+            pickupText.gameObject.SetActive(true);
+            pickupAllowed = false;
+        }
+    }
+
+    private void PickUp()
+    {
+        junkInventory.AddItem(item, 1);
+        Destroy(gameObject);
+        pickupText.gameObject.SetActive(false);
+    }
 }
